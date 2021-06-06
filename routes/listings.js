712264222b3c1e509859ff68835f13aee9725474
Joseph Choi <jchoi192@macbook-pro.myfiosgateway.com>
2021-06-06 15:14:22 -0400

    wip: defines router and controller

--- routes/listings.js
+++ routes/listings.js
@@ -1,12 +1,20 @@
 var express = require('express');
+const { response } = require('../app');
 var router = express.Router();
+var Listing = require('../models/listing');
 
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
   res.status(200).json([
     {title: 'Joe',
     price: 10}
   ])
+});
+
+router.post('/', function(req, res) {
+  Listing.create(req.body, function(err, listing) {
+    response.status(201).json(listing);
+  });
 });
 
 module.exports = router;
