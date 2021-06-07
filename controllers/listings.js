fe2e1d18ca34a2fd88457612dd9997a59bd168e6
Joseph Choi <jchoi192@macbook-pro.myfiosgateway.com>
2021-06-07 14:20:11 -0400

    feat: adds edit action

---
+++ controllers/listings.js
@@ -0,0 +1,30 @@
+var Listing = require('../models/listing');
+
+module.exports = {
+    create,
+    index,
+    update,
+    delete: deleteListing
+};
+
+function index(req, res) {
+    Listing.find({}, function (err, listings) {
+        res.status(200).json(listings)
+    });
+}
+
+function create(req, res) {
+    Listing.create(req.body, function (err, listing) {
+        res.status(201).json(listing);
+    });
+}
+
+function update(req, res) {
+    Listing.findByIdAndUpdate(req.params.id, req.body, function () {
+        index(req, res)
+    })
+}
+
+function deleteListing(req, res) {
+    Listing.findByIdAndDelete()
+}
--- routes/listings.js
+++ routes/listings.js
@@ -1,19 +1,11 @@
 var express = require('express');
-const { response } = require('../app');
 var router = express.Router();
-var Listing = require('../models/listing');
+var listingsCtrl = require('../controllers/listings')
 
 
-router.get('/', function(req, res, next) {
-  Listing.find({}, function(err, listings) {
-    res.status(200).json(listings)
-  });
-});
 
-router.post('/', function(req, res) {
-  Listing.create(req.body, function(err, listing) {
-    res.status(201).json(listing);
-  });
-});
+router.get('/', listingsCtrl.index);
+router.post('/', listingsCtrl.create);
+router.put('/:id', listingsCtrl.update)
 
 module.exports = router;
